39e968396f646a763aca027d6d4d641bb25ac88e
Rodrigo Spósito <rcssposito@gmail.com>
2019-05-09 23:51:24 -0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,6 @@
+window.onload = function() {
+    
+    var openCurtain = document.body;
+    openCurtain.classList.add("loaded");
+}
+
---
+++ sites/dn/js/main.JS
@@ -0,0 +1,97 @@
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("slide");
+  var dots = document.getElementsByClassName("demo");
+  var captionText = document.getElementById("caption");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  captionText.innerHTML = dots[slideIndex-1].alt;
+}
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+// Integração com a API do correio para preencher o endereço a partir do CEP
+
+function limpa_formulário_cep() {
+  //Limpa valores do formulário de cep.
+  document.getElementById('rua').value=("");
+  document.getElementById('bairro').value=("");
+  document.getElementById('cidade').value=("");
+  document.getElementById('uf').value=("");
+  document.getElementById('ibge').value=("");
+}
+
+function meu_callback(conteudo) {
+  if (!("erro" in conteudo)) {
+      //Atualiza os campos com os valores.
+      document.getElementById('rua').value=(conteudo.logradouro);
+      document.getElementById('bairro').value=(conteudo.bairro);
+      document.getElementById('cidade').value=(conteudo.localidade);
+      document.getElementById('uf').value=(conteudo.uf);
+      document.getElementById('ibge').value=(conteudo.ibge);
+  } //end if.
+  else {
+      //CEP não Encontrado.
+      limpa_formulário_cep();
+      alert("CEP não encontrado.");
+  }
+}
+
+function pesquisacep(valor) {
+
+  //Nova variável "cep" somente com dígitos.
+  var cep = valor.replace(/\D/g, '');
+
+  //Verifica se campo cep possui valor informado.
+  if (cep != "") {
+
+      //Expressão regular para validar o CEP.
+      var validacep = /^[0-9]{8}$/;
+
+      //Valida o formato do CEP.
+      if(validacep.test(cep)) {
+
+          //Preenche os campos com "..." enquanto consulta webservice.
+          document.getElementById('rua').value="...";
+          document.getElementById('bairro').value="...";
+          document.getElementById('cidade').value="...";
+          document.getElementById('uf').value="...";
+
+          //Cria um elemento javascript.
+          var script = document.createElement('script');
+
+          //Sincroniza com o callback.
+          script.src = 'https://viacep.com.br/ws/'+ cep + '/json/?callback=meu_callback';
+
+          //Insere script no documento e carrega o conteúdo.
+          document.body.appendChild(script);
+
+      } //end if.
+      else {
+          //cep é inválido.
+          limpa_formulário_cep();
+          alert("Formato de CEP inválido.");
+      }
+  } //end if.
+  else {
+      //cep sem valor, limpa formulário.
+      limpa_formulário_cep();
+  }
+};
